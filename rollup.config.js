d63ce0d602acc89595297530aeba64f4fb49e974
Eshwar Rachala <eshwarrachala@Eshwars-MBP.T-mobile.com>
2016-12-30 14:11:45 -0500

    updated d3 module

--- rollup.config.js
+++ rollup.config.js
@@ -5,5 +5,5 @@ export default {
     format: "umd",
     moduleName: "d3",
     plugins: [node()],
-    dest: "d3.js"
+    dest: "build/d3-er.js"
 };
--- test/d3-test.js
+++ test/d3-test.js
@@ -1,5 +1,5 @@
 var tape = require("tape"),
-    d3 = require("../d3");
+    d3 = require("../build/d3-er");
 
 tape("d3 returns custom function bullet.", function (test) {
     test.equal(d3.bullet(), 'Hello World');
