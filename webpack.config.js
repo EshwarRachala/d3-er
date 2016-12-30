2a3f1087394e476cc25a726697338cbbc83f4c68
Eshwar Rachala <eshwarrachala@Eshwars-MBP.T-mobile.com>
2016-12-30 14:17:26 -0500

    added webpack for client

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: path.join(__dirname, 'src'),
+            loader: 'babel-loader'
+        }]
+    }
+}
